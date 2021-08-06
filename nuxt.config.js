9082ce8d635e4896f42aa95b247039ac1a314cff
Emmanuel Narcisse <epokweb@hotmail.fr>
2021-08-06 09:23:57 +0400

    Fix(ApiLink): heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
   },
   strapi: {},
   axios: {
-    baseURL: "http://localhost:3000/api",
+    baseURL: " https://trap-one-api.herokuapp.com/api",
   },
   auth: {
     redirect: {
