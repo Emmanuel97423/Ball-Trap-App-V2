5d06f518d7e576a8d4d3475d614ee76b9188b002
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-04-06 11:29:30 +0400

    Feature(home-page): top slider 3 slide OK

---
+++ components/carousel/Carousel.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      fade="true"
+      background="#fff"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide img-src="https://picsum.photos/id/237/2000/600">
+        <div class="banner_text_one">
+          <h1>Lorem ipsum<span>Lorem</span></h1>
+          <h3>Economisez 50%</h3>
+          <nuxt-link to="#" class="theme-btn-one bg-black btn_md"
+            >Achetez maintenant</nuxt-link
+          >
+        </div>
+      </b-carousel-slide>
+      <b-carousel-slide img-src="https://picsum.photos/2000/600?grayscale">
+        <!-- <div class="banner_text_one">
+          <h1>Lorem ipsum<span>Lorem</span></h1>
+          <h3>Economisez 50%</h3>
+          <nuxt-link to="#" class="theme-btn-one bg-black btn_md"
+            >Achetez maintenant</nuxt-link
+          >
+        </div> -->
+      </b-carousel-slide>
+      <!--Slide 2-->
+      <b-carousel-slide img-src="https://picsum.photos/seed/picsum/2000/600">
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    },
+  },
+};
+</script>
+<style scoped>
+.banner_text_one h1 {
+  color: #fff;
+  margin: 0 0 20px 0;
+}
+.banner_text_one :nth-child(3) {
+  margin: 20px 0 50px 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-page">
     <!-- Banner Area -->
-    <section id="banner_one">
+    <!-- <section id="banner_one">
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
@@ -14,8 +14,12 @@
             </div>
           </div>
         </div>
+        
       </div>
-    </section>
+    </section> -->
+    <div id="carousel-home">
+      <Carousel />
+    </div>
 
     <!-- Product variation -->
     <section id="product_variation_one" class="pt-15">
@@ -355,7 +359,8 @@ import ProductBox1 from "~/components/product-box/ProductBox1";
 import Timer from "../components/widgets/Timer";
 import InstagramArea from "../components/instagram/InstagramArea";
 import BlogItem1 from "~/components/blog/BlogItem1";
-import carousel from "vue-owl-carousel";
+// import carousel from "vue-owl-carousel";
+import Carousel from "~/components/carousel/Carousel";
 
 export default {
   name: "Home",
@@ -364,90 +369,92 @@ export default {
     Timer,
     InstagramArea,
     BlogItem1,
-    carousel,
+    Carousel,
   },
 
   data() {
     return {
       enabled: false,
       title: "Home",
+      slide: 0,
+      sliding: null,
 
       // Blog Items Data
-      blogItems: [
-        {
-          id: 1,
-          blogThumb: require("assets/img/blog/post1.png"),
-          blogTitle:
-            "This Designer Bronzer Has Even The Drugstore-Beauty-Buyers Splurging!",
-          blogDescription:
-            "Today kicks off early access to the Sephora Spring Sales Event so I wanted to share some of my top recent beauty buys I’ve been",
-          blogPublishDate: "24 February 2021",
-        },
-        {
-          id: 2,
-          blogThumb: require("assets/img/blog/post2.png"),
-          blogTitle: "4 Fresh Ways To Style Leather Shorts For Spring",
-          blogDescription:
-            "We spent spring break this year in California with Cody’s family and it was such a fun getaway. Cody’s family always goes hard on vacation",
-          blogPublishDate: "29 jan 2018",
-        },
-        {
-          id: 3,
-          blogThumb: require("assets/img/blog/post3.png"),
-          blogTitle: "Shopbop Spring Sale Selects All Under Around $100!",
-          blogDescription:
-            "STRAIGHT LEG DENIM (UNDER $100) – Love all the Ribcage Levi’s styles! They are all really flattering. but since these are wider leg I stuck with my usual size (25).",
-          blogPublishDate: "24 February 2021",
-        },
-        {
-          id: 4,
-          blogThumb: require("assets/img/blog/post4.png"),
-          blogTitle: "This Made Me Splurge on The Apple Watch",
-          blogDescription:
-            "From our favourite UK influencers to the best missives from Milan and the coolest New Yorkers, read on some of the best fashion blogs out there.",
-          blogPublishDate: "21 February 2019",
-        },
-        {
-          id: 5,
-          blogThumb: require("assets/img/blog/post5.png"),
-          blogTitle: "This Designer Bronzer Has Even Buyers Splurging!",
-          blogDescription:
-            "Today kicks off early access to the Sephora Spring Sales Event so I wanted to share some of my top recent beauty buys",
-          blogPublishDate: "24 February 2021",
-        },
-        {
-          id: 6,
-          blogThumb: require("assets/img/blog/post6.png"),
-          blogTitle: "4 Tips for A Colorful Easter Tablescape",
-          blogDescription:
-            "Spring is all about the colors! Especially after what feels like an endless winter, I catch myself craving more color than usual.",
-          blogPublishDate: "24 February 2021",
-        },
-        {
-          id: 7,
-          blogThumb: require("assets/img/blog/post7.png"),
-          blogTitle: "Hawaii Couples Trip Guide and Spring Break Faves",
-          blogDescription:
-            "After every trip to Hawaii, I always have a few DMs asking where we stayed, our favorite beaches, etc. Especially with spring break around the corner.",
-          blogPublishDate: "24 February 2021",
-        },
-        {
-          id: 8,
-          blogThumb: require("assets/img/blog/post8.png"),
-          blogTitle: "If You Struggle To Hit Your Goals, Try This Instead",
-          blogDescription:
-            "This year, instead of setting grand, lofty goals and New Years Resolutions, I realized that I respond better to smaller targets that I can cross",
-          blogPublishDate: "24 February 2021",
-        },
-        {
-          id: 9,
-          blogThumb: require("assets/img/blog/post9.png"),
-          blogTitle: "The Luxury Winter Accessory That’s Trending Now",
-          blogDescription:
-            "No matter what you spend on your wardrobe, there are three pieces that can always elevate your look – shoes, handbags, and sunglasses.",
-          blogPublishDate: "24 February 2021",
-        },
-      ],
+      // blogItems: [
+      //   {
+      //     id: 1,
+      //     blogThumb: require("assets/img/blog/post1.png"),
+      //     blogTitle:
+      //       "This Designer Bronzer Has Even The Drugstore-Beauty-Buyers Splurging!",
+      //     blogDescription:
+      //       "Today kicks off early access to the Sephora Spring Sales Event so I wanted to share some of my top recent beauty buys I’ve been",
+      //     blogPublishDate: "24 February 2021",
+      //   },
+      //   {
+      //     id: 2,
+      //     blogThumb: require("assets/img/blog/post2.png"),
+      //     blogTitle: "4 Fresh Ways To Style Leather Shorts For Spring",
+      //     blogDescription:
+      //       "We spent spring break this year in California with Cody’s family and it was such a fun getaway. Cody’s family always goes hard on vacation",
+      //     blogPublishDate: "29 jan 2018",
+      //   },
+      //   {
+      //     id: 3,
+      //     blogThumb: require("assets/img/blog/post3.png"),
+      //     blogTitle: "Shopbop Spring Sale Selects All Under Around $100!",
+      //     blogDescription:
+      //       "STRAIGHT LEG DENIM (UNDER $100) – Love all the Ribcage Levi’s styles! They are all really flattering. but since these are wider leg I stuck with my usual size (25).",
+      //     blogPublishDate: "24 February 2021",
+      //   },
+      //   {
+      //     id: 4,
+      //     blogThumb: require("assets/img/blog/post4.png"),
+      //     blogTitle: "This Made Me Splurge on The Apple Watch",
+      //     blogDescription:
+      //       "From our favourite UK influencers to the best missives from Milan and the coolest New Yorkers, read on some of the best fashion blogs out there.",
+      //     blogPublishDate: "21 February 2019",
+      //   },
+      //   {
+      //     id: 5,
+      //     blogThumb: require("assets/img/blog/post5.png"),
+      //     blogTitle: "This Designer Bronzer Has Even Buyers Splurging!",
+      //     blogDescription:
+      //       "Today kicks off early access to the Sephora Spring Sales Event so I wanted to share some of my top recent beauty buys",
+      //     blogPublishDate: "24 February 2021",
+      //   },
+      //   {
+      //     id: 6,
+      //     blogThumb: require("assets/img/blog/post6.png"),
+      //     blogTitle: "4 Tips for A Colorful Easter Tablescape",
+      //     blogDescription:
+      //       "Spring is all about the colors! Especially after what feels like an endless winter, I catch myself craving more color than usual.",
+      //     blogPublishDate: "24 February 2021",
+      //   },
+      //   {
+      //     id: 7,
+      //     blogThumb: require("assets/img/blog/post7.png"),
+      //     blogTitle: "Hawaii Couples Trip Guide and Spring Break Faves",
+      //     blogDescription:
+      //       "After every trip to Hawaii, I always have a few DMs asking where we stayed, our favorite beaches, etc. Especially with spring break around the corner.",
+      //     blogPublishDate: "24 February 2021",
+      //   },
+      //   {
+      //     id: 8,
+      //     blogThumb: require("assets/img/blog/post8.png"),
+      //     blogTitle: "If You Struggle To Hit Your Goals, Try This Instead",
+      //     blogDescription:
+      //       "This year, instead of setting grand, lofty goals and New Years Resolutions, I realized that I respond better to smaller targets that I can cross",
+      //     blogPublishDate: "24 February 2021",
+      //   },
+      //   {
+      //     id: 9,
+      //     blogThumb: require("assets/img/blog/post9.png"),
+      //     blogTitle: "The Luxury Winter Accessory That’s Trending Now",
+      //     blogDescription:
+      //       "No matter what you spend on your wardrobe, there are three pieces that can always elevate your look – shoes, handbags, and sunglasses.",
+      //     blogPublishDate: "24 February 2021",
+      //   },
+      // ],
     };
   },
 
@@ -464,6 +471,7 @@ export default {
       ],
     };
   },
+
   computed: {
     productItems() {
       return this.$store.getters["products/productItems"];
@@ -484,5 +492,18 @@ h1 {
 }
 .banner_text_one h1 {
   color: black;
+}
+#carousel-home {
+  height: auto;
+}
+/* @media only screen and (max-width:768px) {
+  #carousel-home {
+    height: 600px;
+  }
+} */
+@media only screen and (max-width: 501px) {
+  #carousel-home {
+    display: none;
+  }
 }
 </style>
