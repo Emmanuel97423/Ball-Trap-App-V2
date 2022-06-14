76f9126f81d388935b89b14b6da855fde85e85e0
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-06-14 16:29:06 +0400

    Feature(home-page): banner modification

--- components/carousel/Carousel.vue
+++ components/carousel/Carousel.vue
@@ -14,19 +14,30 @@
       img-width="1024px"
       img-height="400px"
     >
-      <!-- Text slides with image -->
+      <!--Slide 1-->
       <b-carousel-slide
         class="carousel-slide-img"
-        img-src="@/assets/img/slide/slide-1.jpg"
-        fade="true"
+        img-src="@/assets/img/slide/slide-4.png"
       >
-        <nuxt-link to="/shop/shop-2">
-          <img
-            id="visit-card"
-            :src="require('@/assets/img/carte-de-visite.png')"
-            alt="img"
-          />
-        </nuxt-link>
+      </b-carousel-slide>
+
+      <!--Slide 2-->
+      <b-carousel-slide
+        class="carousel-slide-img"
+        img-src="@/assets/img/slide/slide-3.png"
+      >
+        <!-- <div
+          id="visit-card-div"
+          class="product_variation_one_boxed img-zoom-hover"
+        >
+          <nuxt-link to="/shop/shop-2">
+            <img
+              id="visit-card"
+              :src="require('@/assets/img/carte-de-visite.png')"
+              alt="img"
+            />
+          </nuxt-link>
+        </div> -->
 
         <!-- <div class="banner_text_one">
           <h1>Lorem ipsum<span>Lorem</span></h1>
@@ -36,6 +47,30 @@
           >
         </div> -->
       </b-carousel-slide>
+      <!--Slide 2-->
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        class="carousel-slide-img"
+        img-src="@/assets/img/slide/slide-1.jpg"
+        fade="true"
+      >
+        <!-- <nuxt-link to="/shop/shop-2">
+          <img
+            id="visit-card"
+            :src="require('@/assets/img/carte-de-visite.png')"
+            alt="img"
+          />
+        </nuxt-link> -->
+
+        <!-- <div class="banner_text_one">
+          <h1>Lorem ipsum<span>Lorem</span></h1>
+          <h3>Economisez 50%</h3>
+          <nuxt-link to="#" class="theme-btn-one bg-black btn_md"
+            >Achetez maintenant</nuxt-link
+          >
+        </div> -->
+      </b-carousel-slide>
+      <!--Slide 3-->
       <b-carousel-slide
         class="carousel-slide-img"
         img-src="@/assets/img/slide/slide-2.jpg"
@@ -61,7 +96,7 @@
           >
         </div> -->
       </b-carousel-slide>
-      <!--Slide 2-->
+
       <!-- <b-carousel-slide
         class="carousel-slide-img"
         img-src="@/assets/img/slide/slide-1.jpg"
