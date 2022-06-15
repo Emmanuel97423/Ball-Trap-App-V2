2f1619c41a976bebf3eaa6aae4a73fe41df569f7
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-06-15 17:19:10 +0400

    e8951f7

---
+++ _nuxt/f6219a8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{813:function(n,t,e){"use strict";e.r(t);var l={layout:function(n){return"admin"}},r=e(40),component=Object(r.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
