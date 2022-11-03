0967c34d0598e2fffb9e3a997e0fe2bcea22e1e5
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-11-03 18:05:31 +0400

    Test scalingo api for 30 days

--- nuxt.config.js
+++ nuxt.config.js
@@ -126,7 +126,8 @@ export default {
   axios: {
     // baseURL: " http://localhost:8080/api",
     // baseURL: "https://trap-one-api.herokuapp.com/api",
-    baseURL: "https://exo-trap-api.onrender.com/api",
+    // baseURL: "https://exo-trap-api.onrender.com/api",
+    baseURL: "https://exo-trap-api.osc-fr1.scalingo.io/api",
     proxy: false
   },
   auth: {
